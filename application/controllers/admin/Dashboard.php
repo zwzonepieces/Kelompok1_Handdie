d027db1d17e17bb628466c9b73b13ed485b7c6bd
zwzonepieces <riskimpw9988@gmail.com>
2021-11-29 20:45:03 +0700

    tambahan

---
+++ application/controllers/admin/Dashboard.php
@@ -0,0 +1,21 @@
+<?php
+class Dashboard extends CI_Controller{
+	function __construct(){
+		parent::__construct();
+		if(!isset($_SESSION['logged_in'])){
+            $url=base_url('administrator');
+            redirect($url);
+        };
+		$this->load->model('m_pengunjung');
+	}
+	function index(){
+			$x['visitor'] = $this->m_pengunjung->statistik_pengujung();
+			$this->load->view('admin/v_dashboard',$x);
+	
+	}
+	function super(){
+		$x['visitor'] = $this->m_pengunjung->statistik_pengujung();
+		$this->load->view('admin/v_admin',$x);
+	}
+	
+}
