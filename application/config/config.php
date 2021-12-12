0418f49fa8c0609574d361eab65644583ca50f41
zwzonepieces <90605840+zwzonepieces@users.noreply.github.com>
2021-12-12 10:58:50 +0700

    menghilangkan index.php

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/handdie';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
